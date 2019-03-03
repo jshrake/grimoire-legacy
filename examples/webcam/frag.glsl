cebc4aa344149cec01ee82c3cc604bd88d8d051a
Justin Shrake <justinshrake@gmail.com>
2019-03-03 11:24:45 -0800

    Add webcam example

--- examples/webcam/frag.glsl
+++ examples/webcam/frag.glsl
@@ -1,10 +1,8 @@
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   // Normalized pixel coordinates (from 0 to 1)
-  vec2 uv = fragCoord / iResolution.xy;
-  // Time varying pixel color
-  vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
+  vec2 uv = (iResolution.xy - fragCoord) / iResolution.xy;
   // Output to screen
-  fragColor = vec4(col, 1.0);
+  fragColor = texture(iChannel0, uv);
 }
 
 out vec4 fragColor;
