47974216d790af1d43289cc023d77bb85ce26e54
Justin Shrake <justinshrake@gmail.com>
2019-03-12 06:30:42 -0700

    git checkout --theirs

--- examples/scene-0001/ground.frag.glsl
+++ examples/scene-0001/ground.frag.glsl
@@ -1,11 +1,10 @@
 in vec4 v_color;
 in vec2 v_uv;
 out vec4 fragColor;
-
 void main() {
   // ground plane w/ checkerboard pattern
-  vec2 pos = floor(v_uv * 20.0);
+  vec2 pos = floor(v_uv * 2000.0);
   float pattern = mod(pos.x + mod(pos.y, 2.0), 2.0);
   fragColor = pattern * vec4(1.);
-  fragColor.a = 1.0;
+  fragColor.a = 1.;
 }
