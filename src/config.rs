d1fef2029e3b9ae71c768358bd5b273e4bd1b0de
Justin Shrake <justinshrake@gmail.com>
2019-04-06 17:49:20 -0700

    Squashed goodies from issue-34
    - Users can now specify a different format for each color attachment of a
    buffer. Additionally, we simplify the buffer config schema, by
    eliminating the attachment count and format keys, and roll it all into
    the buffer key. This commit should be sort-of backwards compatible. Any
    config that specified buffer::format will need to be updated, but that's
    OK as I think we should get a parse errror/warning in those cases.
    - Enable SRGB framebuffers. This was already supposed to be enabled,
    but it looks like I disabled it at some point.
    - Configure per-pass depth writing
    - Make the depth clear value configurable
    - Fix triangle strip draw count calculation
    - Update the SPEC

--- src/config.rs
+++ src/config.rs
@@ -170,22 +170,19 @@ pub struct PassConfig {
     pub uniform_to_channel: BTreeMap<String, ChannelConfig>,
     // render pass settings
     pub buffer: Option<String>,
-    pub clear: Option<[f32; 4]>,
+    pub clear: Option<ClearConfig>,
     pub blend: Option<BlendConfig>,
-    pub depth: Option<DepthFuncConfig>,
+    pub depth: Option<DepthTestConfig>,
     #[serde(default)]
     pub disable: bool,
 }
 
 #[derive(Debug, Deserialize, PartialEq, Clone)]
 pub struct BufferConfig {
-    pub buffer: bool,
-    #[serde(default = "default_buffer_config_attachments")]
-    pub attachments: usize,
+    #[serde(default = "default_buffer_config_format")]
+    pub buffer: BufferFormatConfig,
     #[serde(default = "default_buffer_config_components")]
     pub components: usize,
-    #[serde(default = "default_buffer_config_format")]
-    pub format: BufferFormat,
     #[serde(default = "default_buffer_depth_config_format")]
     pub depth: BufferDepthConfig,
     pub width: Option<u32>,
@@ -193,7 +190,25 @@ pub struct BufferConfig {
     pub scale: Option<f32>,
 }
 
+impl BufferConfig {
+    pub fn attachment_count(&self) -> usize {
+        match &self.buffer {
+            BufferFormatConfig::Dumb(_) => 1,
+            BufferFormatConfig::Simple(_) => 1,
+            BufferFormatConfig::Complete(v) => v.len(),
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, PartialEq, Clone)]
+#[serde(untagged)]
+pub enum BufferFormatConfig {
+    Dumb(bool),
+    Simple(BufferFormat),
+    Complete(Vec<BufferFormat>),
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 pub enum BufferFormat {
     U8,
@@ -201,14 +216,14 @@ pub enum BufferFormat {
     F32,
 }
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 #[serde(untagged)]
 pub enum BufferDepthConfig {
     Simple(bool),
     Complete(BufferDepthFormat),
 }
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 pub enum BufferDepthFormat {
     U16,
@@ -223,7 +238,32 @@ pub struct DrawConfig {
     pub count: u32,
 }
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
+#[serde(untagged)]
+pub enum DepthTestConfig {
+    Simple(DepthFuncConfig),
+    Complete { func: DepthFuncConfig, write: bool },
+}
+
+impl Default for DepthTestConfig {
+    fn default() -> Self {
+        DepthTestConfig::Complete {
+            func: DepthFuncConfig::Less,
+            write: true,
+        }
+    }
+}
+
+impl DepthTestConfig {
+    pub fn func(&self) -> DepthFuncConfig {
+        *(match self {
+            DepthTestConfig::Simple(func) => func,
+            DepthTestConfig::Complete { func, .. } => func,
+        })
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 pub enum DepthFuncConfig {
     #[serde(rename = "never")]
     Never,
@@ -244,12 +284,31 @@ pub enum DepthFuncConfig {
 }
 
 #[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct BlendConfig {
+#[serde(untagged)]
+pub enum ClearDepthConfig {
+    Simple(f32),
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[serde(untagged)]
+pub enum ClearConfig {
+    Color([f32; 4]),
+    ColorDepth([f32; 5]),
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[serde(untagged)]
+pub enum BlendConfig {
+    Simple(BlendSrcDstConfig),
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
+pub struct BlendSrcDstConfig {
     pub src: BlendFactorConfig,
     pub dst: BlendFactorConfig,
 }
 
-#[derive(Debug, Deserialize, PartialEq, Clone)]
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 pub enum BlendFactorConfig {
     #[serde(rename = "zero")]
     Zero,
@@ -423,18 +482,10 @@ impl EffectConfig {
                 // we simply want to check if the user set these to 0
                 let buffer_width = buffer.width.unwrap_or(1);
                 let buffer_height = buffer.height.unwrap_or(1);
-                let attachments = buffer.attachments;
                 if buffer_width == 0 || buffer_height == 0 {
                     self.ok = false;
                     error!(
                             "[TOML] Buffer \"{}\" must specify non-zero value for the width and height properties",
-                            resource_name
-                        );
-                }
-                if attachments == 0 {
-                    self.ok = false;
-                    error!(
-                            "[TOML] Buffer \"{}\" must specify non-zero value for the attachments property",
                             resource_name
                         );
                 }
@@ -469,11 +520,9 @@ impl Default for DrawConfig {
 impl Default for BufferConfig {
     fn default() -> Self {
         Self {
-            buffer: true,
-            attachments: 1,
-            components: 4,
-            format: BufferFormat::F32,
+            buffer: BufferFormatConfig::Simple(BufferFormat::F32),
             depth: BufferDepthConfig::Complete(BufferDepthFormat::U24),
+            components: 4,
             width: None,
             height: None,
             scale: Some(1.0),
@@ -526,16 +575,12 @@ const fn default_flipv() -> bool {
     true
 }
 
-const fn default_buffer_config_attachments() -> usize {
-    1
-}
-
 const fn default_buffer_config_components() -> usize {
     4
 }
 
-const fn default_buffer_config_format() -> BufferFormat {
-    BufferFormat::F32
+const fn default_buffer_config_format() -> BufferFormatConfig {
+    BufferFormatConfig::Simple(BufferFormat::F32)
 }
 
 const fn default_buffer_depth_config_format() -> BufferDepthConfig {
