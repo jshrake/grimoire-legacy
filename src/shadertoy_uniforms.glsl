c886b7ec85090e57371ff8c38c9577f085fdf28a
Justin Shrake <justinshrake@gmail.com>
2019-04-21 09:07:02 -0700

    rust-fmt

--- src/shadertoy_uniforms.glsl
+++ src/shadertoy_uniforms.glsl
@@ -1,10 +1,10 @@
 /*
 BEGIN: grim.glsl
 */
-layout (std140) uniform GRIM_STATE {
-  vec4  iMouse;
-  vec4  iDate;
-  vec3  iWindowResolution;
+layout(std140) uniform GRIM_STATE {
+  vec4 iMouse;
+  vec4 iDate;
+  vec3 iWindowResolution;
   float iTime;
   float iTimeDelta;
   float iFrame;
